656903cf04aef753f1e54622bbe626217006ed5f
jl777 <jameslee777@yahoo.com>
2015-01-17 22:21:12 +0200

    test

--- libjl777/ramchain.h
+++ libjl777/ramchain.h
@@ -4219,6 +4219,7 @@ uint32_t ram_create_block(int32_t verifyflag,struct ramchain_info *ram,struct ma
     ram_setformatstr(formatstr,blocks->format);
     prevhps = ram_get_hpptr(prevblocks,blocknum);
     ram_setfname(fname,ram,blocknum,formatstr);
+    printf("check create.(%s)\n",fname);
     if ( 0 && blocks->format == 'V' )
     {
         if ( _get_blockinfo(blocks->R,ram,blocknum) > 0 )
