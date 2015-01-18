238ea0f033d3d1b8595487ec8d188913febeb5d2
jl777 <jameslee777@yahoo.com>
2015-01-18 15:07:18 +0200

    fixes

--- libjl777/ramchain.h
+++ libjl777/ramchain.h
@@ -132,7 +132,7 @@ struct ramchain_hashtable { char coinstr[16]; struct ramchain_hashptr *table; st
 
 #define MAX_BLOCKTX 0xffff
 struct rawvin { char txidstr[128]; uint16_t vout; };
-struct rawvout { char coinaddr[64],script[128]; uint64_t value; };
+struct rawvout { char coinaddr[64],script[256]; uint64_t value; };
 struct rawtx { uint16_t firstvin,numvins,firstvout,numvouts; char txidstr[128]; };
 
 struct rawblock
@@ -805,13 +805,6 @@ int32_t init_hexbytes_noT(char *hexbytes,unsigned char *message,long len)
 }
 
 // >>>>>>>>>>>>>>  start bitcoind_RPC interface functions
-#define OP_HASH160_OPCODE 0xa9
-#define OP_EQUAL_OPCODE 0x87
-#define OP_DUP_OPCODE 0x76
-#define OP_EQUALVERIFY_OPCODE 0x88
-#define OP_CHECKSIG_OPCODE 0xac
-#define OP_RETURN_OPCODE 0x6a
-
 int32_t _add_opcode(char *hex,int32_t offset,int32_t opcode)
 {
     hex[offset + 0] = hexbyte((opcode >> 4) & 0xf);
@@ -819,96 +812,352 @@ int32_t _add_opcode(char *hex,int32_t offset,int32_t opcode)
     return(offset+2);
 }
 
-void _calc_script(char *script,char *pubkey,int msigmode)
+enum opcodetype
 {
-    int32_t offset,len;
-    offset = 0;
-    len = (int32_t)strlen(pubkey);
-    offset = _add_opcode(script,offset,OP_DUP_OPCODE);
-    offset = _add_opcode(script,offset,OP_HASH160_OPCODE);
-    offset = _add_opcode(script,offset,len/2);
-    memcpy(script+offset,pubkey,len), offset += len;
-    offset = _add_opcode(script,offset,OP_EQUALVERIFY_OPCODE);
-    offset = _add_opcode(script,offset,OP_CHECKSIG_OPCODE);
-    script[offset] = 0;
+    // push value
+    OP_0 = 0x00,
+    OP_FALSE = OP_0,
+    OP_PUSHDATA1 = 0x4c,
+    OP_PUSHDATA2 = 0x4d,
+    OP_PUSHDATA4 = 0x4e,
+    OP_1NEGATE = 0x4f,
+    OP_RESERVED = 0x50,
+    OP_1 = 0x51,
+    OP_TRUE=OP_1,
+    OP_2 = 0x52,
+    OP_3 = 0x53,
+    OP_4 = 0x54,
+    OP_5 = 0x55,
+    OP_6 = 0x56,
+    OP_7 = 0x57,
+    OP_8 = 0x58,
+    OP_9 = 0x59,
+    OP_10 = 0x5a,
+    OP_11 = 0x5b,
+    OP_12 = 0x5c,
+    OP_13 = 0x5d,
+    OP_14 = 0x5e,
+    OP_15 = 0x5f,
+    OP_16 = 0x60,
+    
+    // control
+    OP_NOP = 0x61,
+    OP_VER = 0x62,
+    OP_IF = 0x63,
+    OP_NOTIF = 0x64,
+    OP_VERIF = 0x65,
+    OP_VERNOTIF = 0x66,
+    OP_ELSE = 0x67,
+    OP_ENDIF = 0x68,
+    OP_VERIFY = 0x69,
+    OP_RETURN = 0x6a,
+    
+    // stack ops
+    OP_TOALTSTACK = 0x6b,
+    OP_FROMALTSTACK = 0x6c,
+    OP_2DROP = 0x6d,
+    OP_2DUP = 0x6e,
+    OP_3DUP = 0x6f,
+    OP_2OVER = 0x70,
+    OP_2ROT = 0x71,
+    OP_2SWAP = 0x72,
+    OP_IFDUP = 0x73,
+    OP_DEPTH = 0x74,
+    OP_DROP = 0x75,
+    OP_DUP = 0x76,
+    OP_NIP = 0x77,
+    OP_OVER = 0x78,
+    OP_PICK = 0x79,
+    OP_ROLL = 0x7a,
+    OP_ROT = 0x7b,
+    OP_SWAP = 0x7c,
+    OP_TUCK = 0x7d,
+    
+    // splice ops
+    OP_CAT = 0x7e,
+    OP_SUBSTR = 0x7f,
+    OP_LEFT = 0x80,
+    OP_RIGHT = 0x81,
+    OP_SIZE = 0x82,
+    
+    // bit logic
+    OP_INVERT = 0x83,
+    OP_AND = 0x84,
+    OP_OR = 0x85,
+    OP_XOR = 0x86,
+    OP_EQUAL = 0x87,
+    OP_EQUALVERIFY = 0x88,
+    OP_RESERVED1 = 0x89,
+    OP_RESERVED2 = 0x8a,
+    
+    // numeric
+    OP_1ADD = 0x8b,
+    OP_1SUB = 0x8c,
+    OP_2MUL = 0x8d,
+    OP_2DIV = 0x8e,
+    OP_NEGATE = 0x8f,
+    OP_ABS = 0x90,
+    OP_NOT = 0x91,
+    OP_0NOTEQUAL = 0x92,
+    
+    OP_ADD = 0x93,
+    OP_SUB = 0x94,
+    OP_MUL = 0x95,
+    OP_DIV = 0x96,
+    OP_MOD = 0x97,
+    OP_LSHIFT = 0x98,
+    OP_RSHIFT = 0x99,
+    
+    OP_BOOLAND = 0x9a,
+    OP_BOOLOR = 0x9b,
+    OP_NUMEQUAL = 0x9c,
+    OP_NUMEQUALVERIFY = 0x9d,
+    OP_NUMNOTEQUAL = 0x9e,
+    OP_LESSTHAN = 0x9f,
+    OP_GREATERTHAN = 0xa0,
+    OP_LESSTHANOREQUAL = 0xa1,
+    OP_GREATERTHANOREQUAL = 0xa2,
+    OP_MIN = 0xa3,
+    OP_MAX = 0xa4,
+    
+    OP_WITHIN = 0xa5,
+    
+    // crypto
+    OP_RIPEMD160 = 0xa6,
+    OP_SHA1 = 0xa7,
+    OP_SHA256 = 0xa8,
+    OP_HASH160 = 0xa9,
+    OP_HASH256 = 0xaa,
+    OP_CODESEPARATOR = 0xab,
+    OP_CHECKSIG = 0xac,
+    OP_CHECKSIGVERIFY = 0xad,
+    OP_CHECKMULTISIG = 0xae,
+    OP_CHECKMULTISIGVERIFY = 0xaf,
+    
+    // expansion
+    OP_NOP1 = 0xb0,
+    OP_NOP2 = 0xb1,
+    OP_NOP3 = 0xb2,
+    OP_NOP4 = 0xb3,
+    OP_NOP5 = 0xb4,
+    OP_NOP6 = 0xb5,
+    OP_NOP7 = 0xb6,
+    OP_NOP8 = 0xb7,
+    OP_NOP9 = 0xb8,
+    OP_NOP10 = 0xb9,
+    
+    
+    
+    // template matching params
+    OP_SMALLINTEGER = 0xfa,
+    OP_PUBKEYS = 0xfb,
+    OP_PUBKEYHASH = 0xfd,
+    OP_PUBKEY = 0xfe,
+    
+    OP_INVALIDOPCODE = 0xff,
+};
+
+const char *get_opname(enum opcodetype opcode)
+{
+    switch (opcode)
+    {
+            // push value
+        case OP_0                      : return "0";
+        case OP_PUSHDATA1              : return "OP_PUSHDATA1";
+        case OP_PUSHDATA2              : return "OP_PUSHDATA2";
+        case OP_PUSHDATA4              : return "OP_PUSHDATA4";
+        case OP_1NEGATE                : return "-1";
+        case OP_RESERVED               : return "OP_RESERVED";
+        case OP_1                      : return "1";
+        case OP_2                      : return "2";
+        case OP_3                      : return "3";
+        case OP_4                      : return "4";
+        case OP_5                      : return "5";
+        case OP_6                      : return "6";
+        case OP_7                      : return "7";
+        case OP_8                      : return "8";
+        case OP_9                      : return "9";
+        case OP_10                     : return "10";
+        case OP_11                     : return "11";
+        case OP_12                     : return "12";
+        case OP_13                     : return "13";
+        case OP_14                     : return "14";
+        case OP_15                     : return "15";
+        case OP_16                     : return "16";
+            
+            // control
+        case OP_NOP                    : return "OP_NOP";
+        case OP_VER                    : return "OP_VER";
+        case OP_IF                     : return "OP_IF";
+        case OP_NOTIF                  : return "OP_NOTIF";
+        case OP_VERIF                  : return "OP_VERIF";
+        case OP_VERNOTIF               : return "OP_VERNOTIF";
+        case OP_ELSE                   : return "OP_ELSE";
+        case OP_ENDIF                  : return "OP_ENDIF";
+        case OP_VERIFY                 : return "OP_VERIFY";
+        case OP_RETURN                 : return "OP_RETURN";
+            
+            // stack ops
+        case OP_TOALTSTACK             : return "OP_TOALTSTACK";
+        case OP_FROMALTSTACK           : return "OP_FROMALTSTACK";
+        case OP_2DROP                  : return "OP_2DROP";
+        case OP_2DUP                   : return "OP_2DUP";
+        case OP_3DUP                   : return "OP_3DUP";
+        case OP_2OVER                  : return "OP_2OVER";
+        case OP_2ROT                   : return "OP_2ROT";
+        case OP_2SWAP                  : return "OP_2SWAP";
+        case OP_IFDUP                  : return "OP_IFDUP";
+        case OP_DEPTH                  : return "OP_DEPTH";
+        case OP_DROP                   : return "OP_DROP";
+        case OP_DUP                    : return "OP_DUP";
+        case OP_NIP                    : return "OP_NIP";
+        case OP_OVER                   : return "OP_OVER";
+        case OP_PICK                   : return "OP_PICK";
+        case OP_ROLL                   : return "OP_ROLL";
+        case OP_ROT                    : return "OP_ROT";
+        case OP_SWAP                   : return "OP_SWAP";
+        case OP_TUCK                   : return "OP_TUCK";
+            
+            // splice ops
+        case OP_CAT                    : return "OP_CAT";
+        case OP_SUBSTR                 : return "OP_SUBSTR";
+        case OP_LEFT                   : return "OP_LEFT";
+        case OP_RIGHT                  : return "OP_RIGHT";
+        case OP_SIZE                   : return "OP_SIZE";
+            
+            // bit logic
+        case OP_INVERT                 : return "OP_INVERT";
+        case OP_AND                    : return "OP_AND";
+        case OP_OR                     : return "OP_OR";
+        case OP_XOR                    : return "OP_XOR";
+        case OP_EQUAL                  : return "OP_EQUAL";
+        case OP_EQUALVERIFY            : return "OP_EQUALVERIFY";
+        case OP_RESERVED1              : return "OP_RESERVED1";
+        case OP_RESERVED2              : return "OP_RESERVED2";
+            
+            // numeric
+        case OP_1ADD                   : return "OP_1ADD";
+        case OP_1SUB                   : return "OP_1SUB";
+        case OP_2MUL                   : return "OP_2MUL";
+        case OP_2DIV                   : return "OP_2DIV";
+        case OP_NEGATE                 : return "OP_NEGATE";
+        case OP_ABS                    : return "OP_ABS";
+        case OP_NOT                    : return "OP_NOT";
+        case OP_0NOTEQUAL              : return "OP_0NOTEQUAL";
+        case OP_ADD                    : return "OP_ADD";
+        case OP_SUB                    : return "OP_SUB";
+        case OP_MUL                    : return "OP_MUL";
+        case OP_DIV                    : return "OP_DIV";
+        case OP_MOD                    : return "OP_MOD";
+        case OP_LSHIFT                 : return "OP_LSHIFT";
+        case OP_RSHIFT                 : return "OP_RSHIFT";
+        case OP_BOOLAND                : return "OP_BOOLAND";
+        case OP_BOOLOR                 : return "OP_BOOLOR";
+        case OP_NUMEQUAL               : return "OP_NUMEQUAL";
+        case OP_NUMEQUALVERIFY         : return "OP_NUMEQUALVERIFY";
+        case OP_NUMNOTEQUAL            : return "OP_NUMNOTEQUAL";
+        case OP_LESSTHAN               : return "OP_LESSTHAN";
+        case OP_GREATERTHAN            : return "OP_GREATERTHAN";
+        case OP_LESSTHANOREQUAL        : return "OP_LESSTHANOREQUAL";
+        case OP_GREATERTHANOREQUAL     : return "OP_GREATERTHANOREQUAL";
+        case OP_MIN                    : return "OP_MIN";
+        case OP_MAX                    : return "OP_MAX";
+        case OP_WITHIN                 : return "OP_WITHIN";
+            
+            // crypto
+        case OP_RIPEMD160              : return "OP_RIPEMD160";
+        case OP_SHA1                   : return "OP_SHA1";
+        case OP_SHA256                 : return "OP_SHA256";
+        case OP_HASH160                : return "OP_HASH160";
+        case OP_HASH256                : return "OP_HASH256";
+        case OP_CODESEPARATOR          : return "OP_CODESEPARATOR";
+        case OP_CHECKSIG               : return "OP_CHECKSIG";
+        case OP_CHECKSIGVERIFY         : return "OP_CHECKSIGVERIFY";
+        case OP_CHECKMULTISIG          : return "OP_CHECKMULTISIG";
+        case OP_CHECKMULTISIGVERIFY    : return "OP_CHECKMULTISIGVERIFY";
+            
+            // expanson
+        case OP_NOP1                   : return "OP_NOP1";
+        case OP_NOP2                   : return "OP_NOP2";
+        case OP_NOP3                   : return "OP_NOP3";
+        case OP_NOP4                   : return "OP_NOP4";
+        case OP_NOP5                   : return "OP_NOP5";
+        case OP_NOP6                   : return "OP_NOP6";
+        case OP_NOP7                   : return "OP_NOP7";
+        case OP_NOP8                   : return "OP_NOP8";
+        case OP_NOP9                   : return "OP_NOP9";
+        case OP_NOP10                  : return "OP_NOP10";
+            
+        case OP_INVALIDOPCODE          : return "OP_INVALIDOPCODE";
+            
+            // Note:
+            //  The template matching params OP_SMALLDATA/etc are defined in opcodetype enum
+            //  as kind of implementation hack, they are *NOT* real opcodes.  If found in real
+            //  Script, just let the default: case deal with them.
+            
+        default:
+            return "OP_UNKNOWN";
+    }
 }
 
-int32_t _origconvert_to_bitcoinhex(char *scriptasm)
+struct bitcoin_opcode { UT_hash_handle hh; uint8_t opcode; } *optable;
+int32_t bitcoin_assembler(char *script)
 {
-    //"asm" : "OP_HASH160 db7f9942da71fd7a28f4a4b2e8c51347240b9e2d OP_EQUAL",
-    char *hex,strbuf[8192];
-    int32_t middlelen,offset,len,OP_HASH160_len,OP_EQUAL_len;
-    len = (int32_t)strlen(scriptasm);
-    OP_HASH160_len = (int32_t)strlen("OP_HASH160");
-    OP_EQUAL_len = (int32_t)strlen("OP_EQUAL");
-    if ( strncmp(scriptasm,"OP_HASH160",OP_HASH160_len) == 0 && strncmp(scriptasm+len-OP_EQUAL_len,"OP_EQUAL",OP_EQUAL_len) == 0 )
+    static struct bitcoin_opcode *optable;
+    char hexstr[8192],*hex,*str;
+    struct bitcoin_opcode *op;
+    char *opname,*invalid = "OP_UNKNOWN";
+    int32_t i,j;
+    long len,k;
+    if ( optable == 0 )
     {
-        hex = (len > sizeof(strbuf)-2) ? calloc(1,len+1) : strbuf;
-        offset = 0;
-        offset = _add_opcode(hex,offset,OP_HASH160_OPCODE);
-        middlelen = len - OP_HASH160_len - OP_EQUAL_len - 2;
-        offset = _add_opcode(hex,offset,middlelen/2);
-        memcpy(hex+offset,scriptasm+OP_HASH160_len+1,middlelen);
-        hex[offset+middlelen] = hexbyte((OP_EQUAL_OPCODE >> 4) & 0xf);
-        hex[offset+middlelen+1] = hexbyte(OP_EQUAL_OPCODE & 0xf);
-        hex[offset+middlelen+2] = 0;
-        if ( Debuglevel > 2 )
-            printf("(%s) -> (%s)\n",scriptasm,hex);
-        strcpy(scriptasm,hex);
-        if ( hex != strbuf )
-            free(hex);
-        if ( is_hexstr(scriptasm) != 0 )
-            return((int32_t)(2+middlelen+2));
-    }
-    if ( scriptasm[0] != 0 )
-        printf("cant assembly anything but OP_HASH160 + <key> + OP_EQUAL (%s)\n",scriptasm);
-    strcpy(scriptasm,"nonstandard");
-    return(-1);
-}
-
-int32_t _convert_to_bitcoinhex(char *scriptasm)
-{
-    char *hex,pubkey[512],*endstr,strbuf[8192];
-    int32_t i,j,middlelen,len,OP_HASH160_len,OP_EQUAL_len;
-    len = (int32_t)strlen(scriptasm);
-    OP_HASH160_len = (int32_t)strlen("OP_DUP OP_HASH160");
-    OP_EQUAL_len = (int32_t)strlen("OP_EQUALVERIFY OP_CHECKSIG");
-    if ( strncmp(scriptasm,"OP_DUP OP_HASH160",OP_HASH160_len) == 0 && strncmp(scriptasm+len-OP_EQUAL_len,"OP_EQUALVERIFY OP_CHECKSIG",OP_EQUAL_len) == 0 )
-    {
-        middlelen = len - OP_HASH160_len - OP_EQUAL_len - 2;
-        memcpy(pubkey,scriptasm+OP_HASH160_len+1,middlelen);
-        pubkey[middlelen] = 0;
-        
-        hex = (len > sizeof(strbuf)-2) ? calloc(1,len+1) : strbuf;
-        _calc_script(hex,pubkey,0);
-        strcpy(scriptasm,hex);
-        if ( hex != strbuf )
-            free(hex);
-        if ( is_hexstr(scriptasm) != 0 )
-            return((int32_t)(2+middlelen+2));
-    }
-    else
-    {
-        endstr = scriptasm + strlen(scriptasm) - strlen(" OP_CHECKSIG");
-        if ( strcmp(endstr," OP_CHECKSIG") == 0 )
+        for (i=0; i<0x100; i++)
         {
-            strcpy(endstr,"ac");
-            //printf("NEWSCRIPT.[%s]\n",scriptasm);
-            if ( is_hexstr(scriptasm) != 0 )
-                return((int32_t)strlen(scriptasm));
+            opname = (char *)get_opname(i);
+            if ( strcmp(invalid,opname) != 0 )
+            {
+                op = calloc(1,sizeof(*op));
+                HASH_ADD_KEYPTR(hh,optable,opname,strlen(opname),op);
+                printf("{%-16s %02x} ",opname,i);
+                op->opcode = i;
+            }
         }
-        else if ( strncmp(scriptasm,"OP_RETURN ",strlen("OP_RETURN ")) == 0 )
-        {
-            //printf("OP_RETURN.(%s) -> ",scriptasm);
-            _add_opcode(scriptasm,0,OP_RETURN_OPCODE);
-            for (i=2,j=strlen("OP_RETURN "); j<=len; j++,i++)
-                scriptasm[i] = scriptasm[j];
-            //printf("(%s)\n",scriptasm);
-            if ( is_hexstr(scriptasm) != 0 )
-                return((int32_t)strlen(scriptasm));
-        }
+        printf("bitcoin opcodes\n");
     }
-    return(_origconvert_to_bitcoinhex(scriptasm));
+    len = strlen(script);
+    hex = (len < sizeof(hexstr)-2) ? hexstr : calloc(1,len+1);
+    strcpy(hex,"ffff");
+    str = script;
+    k = 0;
+    script[len + 1] = 0;
+    while ( *str != 0 )
+    {
+        //printf("k.%ld (%s)\n",k,str);
+        for (j=0; str[j]!=0&&str[j]!=' '; j++)
+            ;
+        str[j] = 0;
+        len = strlen(str);
+        if ( is_hexstr(str) != 0 && (len & 1) == 0 )
+        {
+            k = _add_opcode(hex,(int32_t)k,(int32_t)len>>1);
+            strcpy(hex+k,str);
+            k += len;//, printf("%s ",str);
+        }
+        else
+        {
+            HASH_FIND(hh,optable,str,len,op);
+            if ( op != 0 )
+                k = _add_opcode(hex,(int32_t)k,op->opcode);
+                //sprintf(hex+k,"%02x",op->opcode), k += 2, printf("{%s}.%02x ",str,op->opcode);
+        }
+        str += (j+1);
+    }
+    hex[k] = 0;
+    strcpy(script,hex);
+    if ( hex != hexstr )
+        free(hex);
+    //fprintf(stderr,"-> (%s).k%ld\n",script,k);
+    return((is_hexstr(script) != 0 && (strlen(script) & 1) == 0) ? 0 : -1);
 }
 
 cJSON *_script_has_address(int32_t *nump,cJSON *scriptobj)
@@ -953,7 +1202,10 @@ int32_t _extract_txvals(char *coinaddr,char *script,int32_t nohexout,cJSON *txob
         {
             copy_cJSON(script,hexobj);
             if ( nohexout != 0 )
-                _convert_to_bitcoinhex(script);
+            {
+                //fprintf(stderr,"{%s} ",script);
+                bitcoin_assembler(script);
+            }
         }
         return(0);
     }
