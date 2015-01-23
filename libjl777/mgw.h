e5f1f1c489132f1f2949781b04afb3c7081d9448
jl777 <jameslee777@yahoo.com>
2015-01-23 10:16:12 +0200

    guard

--- libjl777/mgw.h
+++ libjl777/mgw.h
@@ -2059,7 +2059,7 @@ char *genmultisig(char *NXTaddr,char *NXTACCTSECRET,char *previpaddr,char *coins
                 if ( msig != 0 )
                 {
                     update_MGW_msig(msig,NXTaddr);
-                    if ( Global_mp->gatewayid == 2 && flag != 0 ) // let the client do this
+                    if ( MGW_initdone != 0 && Global_mp->gatewayid == 2 && flag != 0 ) // let the client do this
                         broadcast_bindAM(refNXTaddr,msig,0);
                     free(msig);
                 }
