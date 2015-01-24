afe69aa4077f6b5b076d3fe5d4119f467c406307
jl777 <jameslee777@yahoo.com>
2015-01-24 06:18:42 +0200

    enable live deposits

--- libjl777/ramchain.h
+++ libjl777/ramchain.h
@@ -7365,7 +7365,7 @@ void *process_ramchains(void *_argcoinstr)
                 ram_update_disp(ram);
         }
         if ( processed == 0 )
-            sleep(60);
+            sleep(10);
         MGW_initdone++;
     }
     printf("process_ramchains: finished launching\n");
