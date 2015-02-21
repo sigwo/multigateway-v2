769b594f38d675dfd85ad7dc349378c03d5ebf32
jl777 <jameslee777@yahoo.com>
2015-02-21 02:40:23 +0200

    unflip

--- libjl777/orders.h
+++ libjl777/orders.h
@@ -746,8 +746,6 @@ char *placequote_func(char *previpaddr,int32_t dir,char *sender,int32_t valid,cJ
     {
         if ( price != 0. && volume != 0. && dir != 0 )
         {
-            if ( (type & _FLIPMASK) != 0 )
-                dir = -dir;
             create_orderbook_tx(dir,&tx,0,nxt64bits,baseid,relid,price,volume,baseamount,relamount);
             save_orderbooktx(nxt64bits,baseid,relid,&tx);
             if ( remoteflag == 0 && (json= gen_InstantDEX_json(1,&tx.iQ,baseid,relid)) != 0 )
