0156b90bb64ce0ddd2896180f01658995f9ab87d
jl777 <jameslee777@yahoo.com>
2015-05-14 13:32:56 +0300

    test

--- libjl777/plugins/ramchain/ramchain_main.c
+++ libjl777/plugins/ramchain/ramchain_main.c
@@ -643,7 +643,7 @@ int32_t ledger_commit(struct ledger_info *ledger,int32_t continueflag)
             break;
         msleep(1000);
     }
-    ledger->DBs.transactions = (continueflag != 0) ? sp_begin(ledger->DBs.env) : 0;
+    ledger->DBs.transactions = 0;//(continueflag != 0) ? sp_begin(ledger->DBs.env) : 0;
     return(err);
 }
 
@@ -664,7 +664,7 @@ void ramchain_update(struct ramchain *ramchain,char *serverport,char *userpass,i
         oldsupply = ledger->voutsum - ledger->spendsum;
         memset(&MEM,0,sizeof(MEM)), MEM.ptr = &ramchain->DECODE, MEM.size = sizeof(ramchain->DECODE);
         if ( ledger->DBs.transactions == 0 )
-            ledger->DBs.transactions = sp_begin(ledger->DBs.env);
+            ledger->DBs.transactions = 0;//sp_begin(ledger->DBs.env);
         if ( (block= ledger_update(dispflag,ledger,&MEM,ramchain->name,serverport,userpass,&ramchain->EMIT,blocknum)) != 0 )
         {
             if ( (allocsize= ledger_finishblock(ledger,&MEM,block)) <= 0 )
