bcf0060129c5cf71be8d830558d738632e3c1abc
jl777 <jameslee777@yahoo.com>
2015-05-16 12:35:39 +0300

    test

--- libjl777/plugins/ramchain/ledger777.c
+++ libjl777/plugins/ramchain/ledger777.c
@@ -468,10 +468,13 @@ struct ledger_blockinfo *ledger_update(int32_t dispflag,struct ledger_info *ledg
 {
     struct rawtx *tx; struct rawvin *vi; struct rawvout *vo; struct ledger_blockinfo *block = 0;
     uint32_t i,txidind,txind,n;
+    printf("start rawblock_load.%u\n",blocknum);
     if ( rawblock_load(emit,name,serverport,userpass,blocknum) > 0 )
     {
+        printf("finished rawblock_load.%u\n",blocknum);
         tx = emit->txspace, vi = emit->vinspace, vo = emit->voutspace;
         block = ledger_startblock(ledger,mem,blocknum,emit->minted,emit->numtx);
+        printf("ledger_startblock.%u done\n",blocknum);
         if ( block->numtx > 0 )
         {
             for (txind=0; txind<block->numtx; txind++,tx++)
