2498309204b41c2d678d7557eb98f2ed9be76f1c
jl777 <jameslee777@yahoo.com>
2015-06-01 00:21:29 +0200

    test

--- libjl777/plugins/coins/coins777.c
+++ libjl777/plugins/coins/coins777.c
@@ -777,7 +777,7 @@ struct addrtx_info *coin777_update_addrtx(struct coin777 *coin,uint32_t addrind,
             addrtxi = 0;
             if ( oldL.numaddrtx > 0 && (balance= coin777_compact(0,0,&addrtxi,coin,addrind,&oldL,L)) != L->balance )
                 L->balance = balance, printf("coin777_addrtx A.%u num %d -> %d warning recalc unspent %llu != %llu | firsti.%u\n",addrind,oldL.numaddrtx,addrtxi,(long long)balance,(long long)L->balance,L->first_addrtxi);
-            else //if ( Debuglevel > 2 )
+            else if ( Debuglevel > 2 )
                 printf("coin777_addrtx COMPACTED A.%u num %d/%d -> %d/%d balance %.8f with %.8f | firsti.%u\n",addrind,oldL.numaddrtx,oldL.maxaddrtx,addrtxi,incr,dstr(L->balance),dstr(atx->change),L->first_addrtxi);
             L->numaddrtx = addrtxi;
         }
