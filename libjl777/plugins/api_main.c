693ccffa783766660a2ad360b830a2de69878ef0
jl777 <jameslee777@yahoo.com>
2015-06-10 08:03:48 +0200

    test

--- libjl777/plugins/api_main.c
+++ libjl777/plugins/api_main.c
@@ -13,11 +13,54 @@ uint32_t _crc32(uint32_t crc,const void *buf,size_t size);
 long _stripwhite(char *buf,int accept);
 #define nn_errstr() nn_strerror(nn_errno())
 
-int main(int argc, char **argv) {
+void process_json(cJSON *json)
+{
+    int32_t pushsock,pullsock,i,len,checklen; uint32_t tag;
+    char endpoint[128],*resultstr,*jsonstr,*apiendpoint = "ipc://SuperNET.api";
+    if ( json != 0 )
+    {
+        len = (int32_t)strlen(name)+1;
+        tag = _crc32(0,name,len);
+        sprintf(endpoint,"ipc://api.%u",tag);
+        cJSON_AddItemToObject(json,"apitag",cJSON_CreateString(endpoint));
+        jsonstr = cJSON_Print(json), _stripwhite(jsonstr,' ');
+        printf("jsonstr.(%s)\r\n",jsonstr);
+        len = (int32_t)strlen(jsonstr)+1;
+        if ( (pushsock= nn_socket(AF_SP,NN_PUSH)) >= 0 )
+        {
+            if ( nn_connect(pushsock,apiendpoint) < 0 )
+                fprintf(stderr,"error connecting to apiendpoint sock.%d type.%d (%s) %s\n",pushsock,NN_PUSH,apiendpoint,nn_errstr());
+            else if ( (checklen= nn_send(pushsock,jsonstr,len,0)) != len )
+                fprintf(stderr,"checklen.%d != len.%d for nn_send to (%s)\n",checklen,len,apiendpoint);
+            else
+            {
+                if ( (pullsock= nn_socket(AF_SP,NN_PULL)) >= 0 )
+                {
+                    if ( nn_bind(pullsock,endpoint) < 0 )
+                        fprintf(stderr,"error binding to sock.%d type.%d (%s) %s\n",pullsock,NN_PULL,endpoint,nn_errstr());
+                    else
+                    {
+                        if ( nn_recv(pullsock,&resultstr,NN_MSG,0) > 0 )
+                        {
+                            printf("%s\n",resultstr);
+                            nn_freemsg(resultstr);
+                        }
+                    }
+                    nn_shutdown(pullsock,0);
+                }
+                nn_shutdown(pushsock,0);
+            }
+        }
+        free_json(json), free(jsonstr);
+    } else printf("JSON parse error.(%s)\n",name);
+}
+
+int main(int argc, char **argv)
+{
     CGI_varlist *varlist;
     const char *name;
     CGI_value  *value;
-    int i; char *jsonstr; cJSON *json;
+    int i; cJSON *json;
     
     fputs("Content-type: text/plain\r\n\r\n", stdout);
     if ((varlist = CGI_get_all(0)) == 0) {
@@ -42,8 +85,7 @@ int main(int argc, char **argv) {
         }
     }
     CGI_free_varlist(varlist);  /* free variable list */
-    jsonstr = cJSON_Print(json), _stripwhite(jsonstr,' ');
-    printf("%s\r\n",jsonstr);
+    process_json(json);
     return 0;
 }
 
@@ -71,42 +113,6 @@ int oldmain(int argc, char **argv)
             }
         }
         CGI_free_varlist(varlist);
-        if ( json != 0 )
-        {
-            len = (int32_t)strlen(name)+1;
-            tag = _crc32(0,name,len);
-            sprintf(endpoint,"ipc://api.%u",tag);
-            cJSON_AddItemToObject(json,"apitag",cJSON_CreateString(endpoint));
-            jsonstr = cJSON_Print(json), _stripwhite(jsonstr,' ');
-            printf("jsonstr.(%s)\r\n",jsonstr);
-            len = (int32_t)strlen(jsonstr)+1;
-            if ( (pushsock= nn_socket(AF_SP,NN_PUSH)) >= 0 )
-            {
-                if ( nn_connect(pushsock,apiendpoint) < 0 )
-                    fprintf(stderr,"error connecting to apiendpoint sock.%d type.%d (%s) %s\n",pushsock,NN_PUSH,apiendpoint,nn_errstr());
-                else if ( (checklen= nn_send(pushsock,jsonstr,len,0)) != len )
-                    fprintf(stderr,"checklen.%d != len.%d for nn_send to (%s)\n",checklen,len,apiendpoint);
-                else
-                {
-                    if ( (pullsock= nn_socket(AF_SP,NN_PULL)) >= 0 )
-                    {
-                        if ( nn_bind(pullsock,endpoint) < 0 )
-                            fprintf(stderr,"error binding to sock.%d type.%d (%s) %s\n",pullsock,NN_PULL,endpoint,nn_errstr());
-                        else
-                        {
-                            if ( nn_recv(pullsock,&resultstr,NN_MSG,0) > 0 )
-                            {
-                                printf("%s\n",resultstr);
-                                nn_freemsg(resultstr);
-                            }
-                        }
-                        nn_shutdown(pullsock,0);
-                    }
-                    nn_shutdown(pushsock,0);
-                }
-            }
-            free_json(json), free(jsonstr);
-        } else printf("JSON parse error.(%s)\n",name);
     }
     return(0);
 }
