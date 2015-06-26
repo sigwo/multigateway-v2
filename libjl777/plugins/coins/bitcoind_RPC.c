a67181ffe4e00ada46445385a89ad5e5c196c05b
jl777 <jameslee777@yahoo.com>
2015-06-26 02:01:19 +0200

    test

--- libjl777/plugins/coins/bitcoind_RPC.c
+++ libjl777/plugins/coins/bitcoind_RPC.c
@@ -278,9 +278,9 @@ void *curl_post(CURL **cHandlep,char *url,char *userpass,char *postfields,char *
     if ( (cHandle= *cHandlep) == NULL )
 		*cHandlep = cHandle = curl_easy_init();
     else curl_easy_reset(cHandle);
-#ifdef DEBUG
+//#ifdef DEBUG
 	curl_easy_setopt(cHandle,CURLOPT_VERBOSE, 1);
-#endif
+//#endif
 	curl_easy_setopt(cHandle,CURLOPT_USERAGENT,"Mozilla / 4.0 (compatible; btc38 PHP bot; )");//"Mozilla/4.0 (compatible; )");
 	curl_easy_setopt(cHandle,CURLOPT_SSL_VERIFYPEER,0);
 	curl_easy_setopt(cHandle,CURLOPT_URL,url);
