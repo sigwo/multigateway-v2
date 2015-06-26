307eadb28af9ef903b800953f19117553ab76b6c
jl777 <jameslee777@yahoo.com>
2015-06-26 11:25:21 +0200

    test

--- libjl777/plugins/InstantDEX/exchanges.h
+++ libjl777/plugins/InstantDEX/exchanges.h
@@ -380,7 +380,7 @@ uint64_t btc38_trade(char **retstrp,struct exchange_info *exchange,char *base,ch
     if ( strcmp(market,"cny") == 0 )
         pricefmt = "%.5f";
     else pricefmt = "%.6f";
-    sprintf(buf,"%s_118484_%s.%u",exchange->apikey,exchange->apisecret,stamp);
+    sprintf(buf,"%s_118484_%s_%u",exchange->apikey,exchange->apisecret,stamp);
     printf("MD5.(%s)\n",buf);
     calc_md5(digest,buf,(int32_t)strlen(buf));
     sprintf(fmtstr,"key=%%s&time=%%u&md5=%%s&type=%%s&mk_type=%%s&coinname=%%s&price=%s&amount=%s",pricefmt,volfmt);
