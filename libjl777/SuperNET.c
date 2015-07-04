ebb3b77bce79674a824ff1e1dd6b46d9f2df3d61
jl777 <jameslee777@yahoo.com>
2015-07-04 05:43:18 +0200

    test

--- libjl777/SuperNET.c
+++ libjl777/SuperNET.c
@@ -626,9 +626,9 @@ void *issue_cgicall(void *_ptr)
         fprintf(stderr,"error connecting to (%s)\n",apitag);
     else
     {
-        if ( strcmp(method,"relay") == 0 || (broadcaststr != 0 && strcmp(broadcaststr,"publicaccess") == 0) || cJSON_str(cJSON_GetObjectItem(ptr->json,"servicename")) != 0 )
+        if ( strcmp(plugin,"relay") == 0 || (broadcaststr != 0 && strcmp(broadcaststr,"publicaccess") == 0) || cJSON_str(cJSON_GetObjectItem(ptr->json,"servicename")) != 0 )
         {
-            //printf("call busdata_sync\n");
+            printf("call busdata_sync\n");
             str = busdata_sync(ptr->jsonstr,broadcaststr);
             //printf("got.(%s)\n",str);
         }
