cd46c4c49325c490bdde2326bf514b076663c1d5
jl777 <jameslee777@yahoo.com>
2015-07-04 02:33:12 +0200

    test

--- libjl777/plugins/busdata777.c
+++ libjl777/plugins/busdata777.c
@@ -649,7 +649,7 @@ int32_t busdata_validate(char *forwarder,char *sender,uint32_t *timestamp,uint8_
     char pubkey[256],hexstr[65],sha[65],datastr[8192]; int32_t valid; cJSON *argjson; bits256 hash;
     *timestamp = *datalenp = 0;
     forwarder[0] = sender[0] = 0;
-    //printf("busdata_validate.(%s)\n",msg);
+    printf("busdata_validate.(%s)\n",msg);
     if ( is_cJSON_Array(json) != 0 && cJSON_GetArraySize(json) == 2 )
     {
         argjson = cJSON_GetArrayItem(json,0);
