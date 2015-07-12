a97408e556b665abf2d2742387d6f9b080f69e65
jl777 <jameslee777@yahoo.com>
2015-07-12 02:50:18 -0500

    test

--- libjl777/plugins/ramchain/ramchain.c
+++ libjl777/plugins/ramchain/ramchain.c
@@ -13,7 +13,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
-#include "./includes/cJSON.h"
+#include "../includes/cJSON.h"
 #include "../utils/system777.c"
 #include "../coins/coins777.c"
 //#include "pass1.c"
